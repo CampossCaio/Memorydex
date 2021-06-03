1551aa8126cd4ce21008ca00b83a35e3297209c4
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-06-03 20:00:16 -0300

    feature: create Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import { container } from './styles.module.scss';
+
+export function Footer() {
+  return (
+    <footer className={container}>
+      <span>Made with ❤️  and ☕ </span>
+    </footer>
+  )
+}
