d844d7dd1e72f3c65a7e3456aa6f2c26bc3611bd
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-06-05 00:29:38 -0300

    feature: add navigation to home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,18 @@
 import logo from '../../assets/logo.png';
+import { useHistory } from 'react-router-dom';
 import { container } from './styles.module.scss';
 
 export function Header() {
+  const history = useHistory();
+
+  function handleNavigateToHome() {
+    history.push('/');
+  }
+
   return(
     <header className={container}>
-      <img src={logo} alt="memorydex"/>
-      <button>Sair</button>
+      <img onClick={handleNavigateToHome} src={logo} alt="memorydex"/>
+      <button onClick={handleNavigateToHome}>Sair</button>
     </header>
   );
 }
