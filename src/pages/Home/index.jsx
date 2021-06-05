b0bc230b924ee37852682cb556d6c207dc8d8352
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-06-05 00:27:09 -0300

    feature: create Home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,24 @@
+import { container } from './styles.module.scss';
+import { useHistory } from 'react-router-dom';
+
+export function Home() {
+  const history = useHistory();
+
+  function handleNavigateToGame() {
+    history.push('/game');
+  }
+
+  return (
+   <div className={container}>
+     <div>
+       <h1>Seja bem vindo, <span>treinador(a)!</span></h1>
+       <p>
+         Alguns pokemons se perderam de seus teinadores.
+         Precisamos  de sua ajuda para encontra-los.<br/>
+         <span> Aceita o desafio</span>?
+       </p>
+       <button onClick={handleNavigateToGame}>Eu aceito!</button>
+     </div>
+   </div>
+  )
+}
